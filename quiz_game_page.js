8df728418840c717f9eb9661951c3f72287b051a
ReginaRen1124 <125526026+ReginaRen1124@users.noreply.github.com>
2023-10-15 18:41:22 -0600

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,50 @@
+player1_name = localStorage.getItem("j1");
+player2_name = localStorage.getItem("j2");
+player1_score = 0;
+player2_score = 0;
+
+function send() {
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    actual_answer = parseInt(num1) * parseInt(num2);
+    console.log(actual_answer);
+
+    question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check()
+{
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer)
+    {
+        if(answer_turn == "player1")
+        {
+            update_player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        }
+        else
+        {
+            update_player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = update_player2_score;
+        }
+    }
+    if(question_turn == "player1")
+    {
+        question_turn = "player2"
+        document.getElementById("player_question").innerHTML = "turno para preguntar - " + player2_name;
+    }
+    else
+    {
+        question_turn = "player1"
+        document.getElementById("player_question").innerHTML = "turno para preguntar - " + player1_name;
+    }
+}
